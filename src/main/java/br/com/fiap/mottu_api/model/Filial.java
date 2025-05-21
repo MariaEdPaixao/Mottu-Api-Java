7942e5715c9600d5bc1b10a873d11c3cf6b35da9
MariaEdPaixao <mariaeduardaalvesdapaixao0807@gmail.com>
2025-05-20 21:21:02 -0300

    feat: endereço na filial

--- src/main/java/br/com/fiap/mottu_api/model/Filial.java
+++ src/main/java/br/com/fiap/mottu_api/model/Filial.java
@@ -1,6 +1,5 @@
 package br.com.fiap.mottu_api.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -8,10 +7,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import br.com.fiap.mottu_api.model.HistoricoMotoFilial;
-import java.util.List;
-import java.util.ArrayList;
-
 
 @Entity
 @Data
@@ -19,14 +14,31 @@ import java.util.ArrayList;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Filial {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank(message = "O nome da filial não pode estar em branco.")
-    @Size( min = 6)
+    @Size(min = 6)
     private String nomeFilial;
 
-    @OneToMany(mappedBy = "filial")
-    @JsonIgnore
-    private List<HistoricoMotoFilial> historicoMotos = new ArrayList<>();
+    @NotBlank(message = "O logradouro é obrigatório.")
+    private String logradouro;
+
+    private int numero;
+
+    private String complemento;
+
+    @NotBlank(message = "O CEP é obrigatório.")
+    private String cep;
+
+    @NotBlank(message = "A cidade é obrigatória.")
+    private String cidade;
+
+    @NotBlank(message = "O estado é obrigatório.")
+    private String estado;
+
+    @NotBlank(message = "O país é obrigatório.")
+    private String pais;
 }
